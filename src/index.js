b54132780d61d289203c9c9d1fe1910f274f824b
Alexander Santiago Cárdenas Padilla <alexander.cardenas10@outlook.com>
2022-06-18 21:25:42 -0500

    Add header component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import NavBar from './components/NavBar';
+import Header from './components/Header';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <NavBar />
+    <Header />
   </React.StrictMode>,
 );
