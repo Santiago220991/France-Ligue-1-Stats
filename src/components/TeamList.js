0f2ab3fdefbbe838281dc9024c640e6bf56d2126
Alexander Santiago Cárdenas Padilla <alexander.cardenas10@outlook.com>
2022-06-18 22:19:22 -0500

    Add useState hook to TeamList component

--- src/components/TeamList.js
+++ src/components/TeamList.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './TeamList.css';
 
-const TeamList = () => (
-  <div className="team-list">
-    <div className="team-list-header">
-      <p>TEAMS</p>
-      <input className="search-input" type="text" placeholder="Search.." />
-    </div>
-    <div className="teams" />
-  </div>
-);
+const TeamList = () => {
+  const [Filter, setFilter] = useState('');
 
+  const onChange = (e) => {
+    const newFilter = e.target.value;
+    setFilter(newFilter);
+    console.log(Filter);
+  };
+
+  return (
+    <div className="team-list">
+      <div className="team-list-header">
+        <p>TEAMS</p>
+        <input className="search-input" onChange={onChange} type="text" placeholder="Search.." />
+      </div>
+      <div className="teams" />
+    </div>
+  );
+};
 export default TeamList;
