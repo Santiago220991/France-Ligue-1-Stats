c7aa923e711428dd4273e1265c2ad15a41d4fc1c
Alexander Santiago Cárdenas Padilla <alexander.cardenas10@outlook.com>
2022-06-18 19:49:12 -0500

    Add styling to NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,13 +2,14 @@ import React from 'react';
 import backbtn from './backbtn-img.png';
 import configbtn from './configbtn-img.png';
 import micbtn from './micbtn-img.png';
+import './NavBar.css';
 
 const NavBar = () => (
-  <div>
-    <img src={backbtn} alt="back-button" />
-    <p>France League 1</p>
-    <img src={micbtn} alt="mic-button" />
-    <img src={configbtn} alt="config-button" />
+  <div className="navbar">
+    <img className="headerbtn" src={backbtn} alt="back-button" />
+    <p className="title">FRANCE LIGUE 1</p>
+    <img className="headerbtn" src={micbtn} alt="mic-button" />
+    <img className="headerbtn" src={configbtn} alt="config-button" />
   </div>
 );
 
